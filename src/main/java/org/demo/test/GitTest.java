c576c8531b6f608d14c8cb696380ab0340ba0243
zcc <zcc@qq.com>
2022-04-27 10:32:28 +0800

    two commit

--- src/main/java/org/demo/test/GitTest.java
+++ src/main/java/org/demo/test/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("dage");
         System.out.println("da");
+        System.out.println("two hello");
     }
 }
