844b4dffbe18a183106d8fd5dbf1f1f60aae24d4
zcc <zcc@qq.com>
2022-04-27 10:30:30 +0800

    first commit

---
+++ src/main/java/org/demo/test/GitTest.java
@@ -0,0 +1,12 @@
+package org.demo.test;
+
+/**
+ * @Author:芯古子
+ * @Date:2022/4/27 10:26
+ **/
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("dage");
+        System.out.println("da");
+    }
+}
